b3a6bb732af49a89b4c8621fcbb5d68faa97a088
jy <jyinteractive.tw@gmail.com>
2025-11-26 14:35:35 +0900

    fixed font scaling

--- index.js
+++ index.js
@@ -79,8 +79,8 @@ function start() {
   let inviteHan = ShowText.createText('請汝做伙來-');
   let inviteLtn = ShowText.createText('you\'ve been invited to join-');
   let partyInfoHan = ShowText.createText('台北 / 一月 17 / 運算子數位藝術節');
-  let partyInfoLtn = ShowText.createText('Taipei / JAN 17 / Operator Digitalfest', 64);
-  let sessionsLtn = ShowText.createText('Hope you had a fun SESSIONS 2025', 64);
+  let partyInfoLtn = ShowText.createText('Taipei / JAN 17 / Operator Digitalfest', 6);
+  let sessionsLtn = ShowText.createText('Hope you had a fun SESSIONS 2025', 6);
   const GREETZ = 'stargaze,wrighter,0b5vr,Session Orgas,Low Score Boy,FL_YANG,jrwei,ananq_0w0,whereischappie,psenough,ocf.tw,g0v.tw,Atsushi Eno,Amos Li'.split(',');
   let greetzLtn = ShowText.createText(GREETZ[0]);
   ShowText.addToRack(greetzLtn);
--- src/text.js
+++ src/text.js
@@ -1,9 +1,9 @@
-function createText(content, size=96) {
+function createText(content, size=9) {
   let text = document.createElement('div');
   text.style.fontFamily = 'sans-serif';
   text.style.fontWeight = '900';
   text.style.textAlign = 'center';
-  text.style.fontSize = `${size}px`;
+  text.style.fontSize = `${size}vh`;
   text.textContent = content;
   return text;
 }
